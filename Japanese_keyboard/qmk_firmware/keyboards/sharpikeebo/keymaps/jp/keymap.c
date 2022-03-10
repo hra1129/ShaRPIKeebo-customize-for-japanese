4ecf2479abdadcb9e9938d1e07efc7b628ac1047
HRA! <hra@mvj.biglobe.ne.jp>
2022-03-10 22:22:34 +0900

    Updated

--- Japanese_keyboard/qmk_firmware/keyboards/sharpikeebo/keymaps/jp/keymap.c
+++ Japanese_keyboard/qmk_firmware/keyboards/sharpikeebo/keymaps/jp/keymap.c
@@ -29,10 +29,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_FN] = LAYOUT(\
 	//	COL00  , COL01  , COL02  , COL03  , COL04  , COL05  , COL06  , COL07  , COL08  , COL09  , COL10  , COL11
 		KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_PGUP, KC_PGDN, /* ROW0 */ \
-		KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_EQL , KC_GRV , KC_GRV , KC_HOME, KC_END , /* ROW1 */ \
-		KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_SCLN, KC_BSPC, KC_MINS, KC_INS , /* ROW2 */ \
-		KC_TAB , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_SLSH, KC_COMM, KC_DOT , KC_ENT , KC_LBRC, KC_RBRC, /* ROW3 */ \
-		KC_LCTL, KC_LSFT, MO(_FN), KC_SPC , KC_LALT, KC_QUOT, KC_DEL , KC_LNG5, KC_F9  , KC_F10 , KC_F11 , KC_F12   /* ROW4 */ \
+		KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_MINS, KC_EQL , KC_P   , KC_HOME, KC_END , /* ROW1 */ \
+		KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_SLSH, KC_SCLN, KC_QUOT, KC_ENT , KC_F5  , KC_INT1, /* ROW2 */ \
+		KC_TAB , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_COMM, KC_DOT , KC_RBRC, KC_BSLS, KC_INT3, KC_INS , /* ROW3 */ \
+		KC_LCTL, KC_LSFT, MO(_FN), KC_SPC , KC_LALT, KC_LBRC, KC_DEL , KC_GRV , KC_F9  , KC_F10 , KC_F11 , KC_F12   /* ROW4 */ \
 	)
 };
 
